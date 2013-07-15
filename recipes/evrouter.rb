57a916fdc1dae5d3af2c4d5ab326fc6f73ebed8c
Jeffrey Peckham <abic@ophymx.com>
2013-07-14 22:58:59 -0700

    Added checksum of evrouter

--- recipes/evrouter.rb
+++ recipes/evrouter.rb
@@ -1,5 +1,6 @@
 remote_file File.join(Chef::Config[:file_cache_path], 'evrouter_0.4_amd64.deb') do
   source 'http://debian.bedroomlan.org/debian/pool/main/e/evrouter/evrouter_0.4_amd64.deb'
+  checksum 'f17bfb2a44db7'
 end
 
 package 'evrouter_0.4_amd64.deb' do
