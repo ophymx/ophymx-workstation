c81fa10c24dc94363a3445f4984d1256ef3e5bb9
Jeffrey Peckham <peckham@pivotallabs.com>
2013-07-18 07:38:26 -0700

    Added EnvyCodeR font

---
+++ recipes/envy-code-r.rb
@@ -0,0 +1,17 @@
+envy_code_r_url = 'http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip'
+envy_code_r_file = 'EnvyCodeR-PR7.zip'
+envy_code_r_cache_file = File.join(Chef::Config[:file_cache_path], envy_code_r_file)
+
+remote_file envy_code_r_cache_file do
+  source envy_code_r_url
+  checksum '9f7e9703aaf2111'  
+end
+
+directory '/usr/local/share/fonts'
+
+execute 'unzip-envy-code-r' do
+  command "unzip #{envy_code_r_cache_file}"
+  cwd '/usr/local/share/fonts'
+  action :run
+  not_if { File.exist?('/usr/local/share/fonts/Envy Code R PR7/Envy Code R.ttf') }
+end
