cce8eddd676943b5f915150de0273d285a2b90e0
Jeffrey Peckham <peckham@pivotallabs.com>
2013-07-18 07:42:06 -0700

    Starting to manage home directory

---
+++ recipes/user-session.rb
@@ -0,0 +1,63 @@
+user_home = File.join('/home', node['ophymx']['user'])
+
+%w(.sv .config .config/terminator .config/awesome).each do |dir|
+  directory File.join(user_home, dir) do
+    owner node['ophymx']['user']
+    group node['ophymx']['group']
+  end
+end
+
+user_scripts = {
+  'xinitrc' => '.xinitrc',
+  'xinitrc.inputs' => '.xinitrc.inputs',
+}
+user_scripts.each_pair do |src, dst|
+  cookbook_file File.join(user_home, dst) do
+    source src
+    mode 00755
+    owner node['ophymx']['user']
+    group node['ophymx']['group']
+  end
+end
+
+sv_services = %w(
+  awesome
+  gnome-keyring
+  gnome-polkit
+  nm-applet
+  parcellite
+  print-applet
+  update-notifier
+  xfce4-power-manager
+  xscreensaver
+)
+sv_services.each do |service|
+  directory File.join(user_home, '.sv', service) do
+    owner node['ophymx']['user']
+    group node['ophymx']['group']
+  end
+  cookbook_file File.join(user_home, '.sv', service, 'run') do
+    source File.join('sv', service, 'run')
+    mode 00755
+    owner node['ophymx']['user']
+    group node['ophymx']['group']
+  end
+end
+
+user_configs = {
+  'synclientrc' => '.synclientrc',
+  'xmodmaprc' => '.xmodmaprc',
+  'xscreensaver' => '.xscreensaver',
+  'xscreensaver-resources' => '.xscreensaver-resources',
+  'terminator.config' => '.config/terminator/config',
+  'awesome.rc.lua' => '.config/awesome/rc.lua',
+  'sv/common.sh' => '.sv/common.sh'
+}
+user_configs.each_pair do |src, dst|
+  cookbook_file File.join(user_home, dst) do
+    source src
+    mode 00644
+    owner node['ophymx']['user']
+    group node['ophymx']['group']
+  end
+end
---
+++ recipes/user.rb
@@ -0,0 +1,27 @@
+include_recipe 'ophymx-workstation::default'
+
+include_recipe 'ophymx-workstation::user-session'
+
+config_files = %w(/usr/share/xsessions/custom.desktop /usr/share/X11/xorg.conf.d/11-evdev-rat5.conf)
+
+config_files.each do |config_file|
+  cookbook_file config_file do
+    mode 00644
+    owner 'root'
+    group 'root'
+  end
+end
+
+scripts = %w(wheel-ctrl vol_ctl dock touchpad-toggle wacom-touch session-control)
+
+scripts.each do |script|
+  cookbook_file File.join('/usr/local/bin', script) do
+    mode 00755
+    owner 'root'
+    group 'root'
+  end
+end
+
+link '/usr/local/bin/undock' do
+  to 'dock'
+end
