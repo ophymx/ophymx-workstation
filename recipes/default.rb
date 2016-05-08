e703871fa5351e52faa1c7d2d1d58efeabc0d903
Jeffrey Peckham <peckham@mombo.com>
2016-05-07 23:45:45 -0700

    Remove evrouter

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,4 @@
 include_recipe 'ophymx-workstation::base_packages'
-include_recipe 'ophymx-workstation::evrouter'
 include_recipe 'ophymx-workstation::envy-code-r'
 include_recipe 'ophymx-workstation::google-chrome'
 include_recipe 'ophymx-workstation::google-talkplugin'
