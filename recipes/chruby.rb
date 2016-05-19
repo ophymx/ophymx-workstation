20d340c260a69d509b8042c040efd1dea048e17f
Jeffrey T. Peckham <abic@ophymx.com>
2016-05-19 00:30:05 -0700

    Fix chruby and ruby-install

--- recipes/chruby.rb
+++ recipes/chruby.rb
@@ -4,13 +4,11 @@ chruby_checksum = '7220a96e355b8a613929881c091ca85ec809153988d7d691299e0a16806b4
 chruby_prefix='/usr/local/chruby'
 chruby_cache_file = File.join(Chef::Config[:file_cache_path], "chruby-#{chruby_version}.tar.gz")
 
-def get_chruby_version
-  chruby_exec = File.join(chruby_prefix, 'run', 'chruby-exec')
-  return nil unless File.executable?(chruby_exec)
-  version = %x{#{chruby_exec} --version}
-  return nil unless version.start_with?('chruby version ')
-  version[15..-1]
-end
+check_chruby_version = Proc.new {
+  chruby_exec = File.join(chruby_prefix, 'bin', 'chruby-exec')
+  File.executable?(chruby_exec) &&
+    %x{#{chruby_exec} --version} == "chruby version #{chruby_version}\n"
+}
 
 remote_file chruby_cache_file do
   source "https://github.com/postmodern/chruby/archive/v#{chruby_version}.tar.gz"
@@ -20,19 +18,19 @@ end
 execute "tar -xzf #{chruby_cache_file}" do
   creates "/tmp/chruby-#{chruby_version}"
   cwd '/tmp'
-  not_if { get_chruby_version == chruby_version }
+  not_if(&check_chruby_version)
 end
 
-directory "/usr/local/chruby" do
+directory '/usr/local/chruby' do
   recursive true
   action :delete
-  not_if { get_chruby_version == chruby_version }
+  not_if(&check_chruby_version)
 end
 
 execute 'install-chruby' do
   command "make PREFIX=#{chruby_prefix} install"
   cwd "/tmp/chruby-#{chruby_version}"
-  not_if { get_chruby_version == chruby_version }
+  not_if(&check_chruby_version)
 end
 
 cookbook_file '/etc/profile.d/chruby.sh' do
--- recipes/ruby_install.rb
+++ recipes/ruby_install.rb
@@ -4,35 +4,33 @@ ruby_install_checksum = '3cc90846ca972d88b601789af2ad9ed0a496447a13cb986a3d74a4d
 ruby_install_prefix='/usr/local/ruby-install'
 ruby_install_cache_file = File.join(Chef::Config[:file_cache_path], "ruby-install-#{ruby_install_version}.tar.gz")
 
-def get_ruby_install_version
-  ruby_install = File.join(ruby_install_prefix, 'run', 'ruby-install')
-  return nil unless File.executable?(ruby_install)
-  version = %x{#{ruby_install} --version}
-  return nil unless version.start_with?('ruby-install: ')
-  version[14..-1]
-end
+check_ruby_install_version = Proc.new {
+  ruby_install = File.join(ruby_install_prefix, 'bin', 'ruby-install')
+  File.executable?(ruby_install) &&
+    %x{#{ruby_install} --version} == "ruby-install: #{ruby_install_version}\n"
+}
 
 remote_file ruby_install_cache_file do
-  source "https://github.com/postmodern/ruby-install/archive/#{ruby_install_version}.tar.gz"
+  source "https://github.com/postmodern/ruby-install/archive/v#{ruby_install_version}.tar.gz"
   checksum ruby_install_checksum
 end
 
 execute "tar -xzf #{ruby_install_cache_file}" do
   creates "/tmp/ruby-install-#{ruby_install_version}"
   cwd '/tmp'
-  not_if { get_ruby_install_version == ruby_install_version }
+  not_if(&check_ruby_install_version)
 end
 
 directory "/usr/local/ruby-install" do
   recursive true
   action :delete
-  not_if { get_ruby_install_version == ruby_install_version }
+  not_if(&check_ruby_install_version)
 end
 
 execute 'install-ruby-install' do
   command "make PREFIX=#{ruby_install_prefix} install"
   cwd "/tmp/ruby-install-#{ruby_install_version}"
-  not_if { get_ruby_install_version == ruby_install_version }
+  not_if(&check_ruby_install_version)
 end
 
 cookbook_file '/etc/profile.d/ruby-install.sh' do
